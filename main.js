0932c21ee6cd2940e187ccefa1c01148176136a5
Keerthana <92567258+PepAsia@users.noreply.github.com>
2022-03-22 17:20:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function view()
+{
+    window.location = "menu.html";
+}
+
+function previous()
+{
+    window.location = "index.html";
+}
+
+function gallery()
+{
+    window.location = "my_gallery.html";
+    console.log("in gallery page");
+}
